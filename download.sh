6a5d32fede757511a29316cee7e24b095d4aba66
gvxhrgi <134945335+gvxhrgi@users.noreply.github.com>
2023-07-23 04:05:18 -0700

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,4 @@
+curl -SsL https://playit-cloud.github.io/ppa/key.gpg | sudo apt-key add -
+sudo curl -SsL -o /etc/apt/sources.list.d/playit-cloud.list https://playit-cloud.github.io/ppa/playit-cloud.list
+sudo apt update
+sudo apt get install screen playit -y
